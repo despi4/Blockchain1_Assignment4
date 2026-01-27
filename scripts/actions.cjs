e32048c9d0da8203a1886966044dd4112bd2050e
despi4 <johapo62@gmail.com>
2026-01-27 20:34:57 +0500

    part 1

--- scripts/actions.cjs
+++ scripts/actions.cjs
@@ -1,7 +1,7 @@
 const { ethers } = require("hardhat");
 
-const TOKEN_ADDRESS = "PASTE_TOKEN_ADDRESS_HERE";
-const NFT_ADDRESS = "PASTE_NFT_ADDRESS_HERE";
+const TOKEN_ADDRESS = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
+const NFT_ADDRESS = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9";
 
 async function main() {
   const [owner, alice, bob] = await ethers.getSigners();
